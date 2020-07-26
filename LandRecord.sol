55762b8c348feb62923c1bf84f96237b17ff4f37
Saikiran Sondarkar <saisondarkar18@gmail.com>
2020-07-26 12:36:45 +0530

    Create LandRecord.sol

---
+++ LandRecord.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.17 < 0.6.12; 
+
+contract LandRecord{
+    
+    string public owner;
+    uint public   value ;
+    string public location;
+    string public area;
+    string public legalId;
+    
+    function LandRecord(string newOwner, uint newValue, string newLocation, string newArea, string newLegalId) public{
+        
+        owner = newOwner;
+        value = newValue;
+        location = newLocation;
+        area = newArea;
+        legalId = newLegalId;
+        
+    }
+    
+    function setLandNewDetails(string newOwner, uint newValue) public{
+        
+        owner = newOwner;
+        value = newValue;
+        
+    }
+    
+    function getLandCurrentDetails() public view returns(string,uint,string,string,string){
+        return(owner, value, location, area, legalId);
+    }
+    
+    
+}
+
+
